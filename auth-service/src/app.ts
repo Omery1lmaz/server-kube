514aa1cabe0b73c34dd3eb48109741fff900b0be
omerfaruk <omerfaruk@badly.ai>
2025-02-27 03:52:26 +0300

    feat: add sigup controller / Auth service

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -2,10 +2,12 @@ import { json } from "body-parser";
 import express from "express";
 import { signinRouter } from "./routes/signin";
 import { errorHandler, NotFoundError } from "@heaven-nsoft/common";
+import { signupRouter } from "./routes/signup";
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
 app.use(signinRouter);
+app.use(signupRouter);
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
