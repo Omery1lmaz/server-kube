acfdde7515c62043719a88fd3c59f6ff51c75683
omerfaruk <omerfaruk@badly.ai>
2025-03-02 03:22:24 +0300

    feat: get user details route and controller

---
+++ auth-service/src/controllers/details.ts
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { User } from "../Models/user";
+import { BadRequestError } from "@heaven-nsoft/common";
+
+export const detailsController = async (req: Request, res: Response) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Lütfen giriş yapın" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(400).json({ message: "Token bulunamadı" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY!) as {
+      id: string;
+    };
+
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
+    }
+
+    return res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    });
+  } catch (error) {
+    console.error("Kimlik doğrulama hatası:", error);
+    return res.status(400).json({ message: "Kimlik doğrulama başarısız" });
+  }
+};
+
+export default detailsController;
---
+++ auth-service/src/routes/details.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { validateRequest } from "@heaven-nsoft/common";
+import detailsController from "../controllers/details";
+
+const router = express.Router();
+
+router.post("/details", validateRequest, detailsController);
+
+export { router as deleteProfileRouter };
